6e851fc6adf51111b469dbff420892949661fb80
lxm <289096620@qq.com>
2020-03-05 17:23:04 +0800

    修改了污染物演进和洪水演进

--- src/components/home/frame.vue
+++ src/components/home/frame.vue
@@ -27,6 +27,10 @@
                          style="position: absolute;
                          z-index: 1;top: -20px;left: -20px;"
                          src="/images/warn/red.gif">
+                    <el-button v-show="bShowFloodAnaly" @click="onHomeBtnClick"
+                               type="success"
+                               icon="el-icon-map-location"
+                               class="location-bar no-print" style="right:10px;top:5px;">回到初始视角</el-button>
                     <div v-show="bShowFloodAnaly" class="location-bar no-print" style="left: 2px; top: 50px;">
                         涉及乡镇(个)：{{flood1}}<br/>
                         漫滩水深(米)：{{flood2}}<br/>
@@ -282,9 +286,10 @@
                 //洪水演进所需的数据
                 bShowFloodEvo:false,        //是否显示流量过程线
                 bbox:[93.412690,32.596075,108.709382,42.793593],//生成数据的边界
-                linePts:[],      //随机生成线的坐标集合
+                linePts:[ [ 105.95996122523042, 34.544616981388629 ], [ 105.9464612259049, 34.556016980998947 ], [ 105.94006122574501, 34.558316980943971 ], [ 105.91466122533535, 34.559316981193774 ], [ 105.90826122517547, 34.557816981268729 ], [ 105.86566122568507, 34.567116981163792 ], [ 105.86756122517045, 34.573916980884007 ], [ 105.87676122584998, 34.585116981163821 ], [ 105.85336122593986, 34.610116981113833 ], [ 105.84526122562511, 34.611716981153791 ], [ 105.83526122582498, 34.609516981323679 ], [ 105.83076122515047, 34.619316980893984 ], [ 105.82446122510549, 34.621616980839065 ], [ 105.79706122509555, 34.615816981368653 ], [ 105.78896122568005, 34.620316981143844 ], [ 105.77806122574503, 34.621916981183801 ], [ 105.7626612251355, 34.622016981298714 ], [ 105.7580612252454, 34.626516981073848 ], [ 105.75816122536031, 34.63631698064421 ], [ 105.75546122585496, 34.638616980589234 ], [ 105.74366122578499, 34.637916980684167 ], [ 105.73826122587491, 34.641716980554236 ], [ 105.73576122570006, 34.666616981288712 ], [ 105.71856122572007, 34.676416980859017 ], [ 105.68946122555519, 34.681116980864033 ], [ 105.68216122526042, 34.684216980829035 ], [ 105.66586122541526, 34.697116981263719 ], [ 105.64406122554516, 34.697916981283697 ], [ 105.63776122550024, 34.701016981248756 ], [ 105.62416122516049, 34.713816980669151 ], [ 105.61056122572006, 34.714616980689186 ], [ 105.5932612256251, 34.71251698097393 ], [ 105.56056122581998, 34.721616980639169 ], [ 105.54596122523043, 34.713416981108821 ], [ 105.53576122520042, 34.698416980958939 ], [ 105.52756122567007, 34.693216981278738 ], [ 105.52306122589493, 34.696216981128828 ], [ 105.51946122535531, 34.704516980774088 ], [ 105.51126122582497, 34.706016980699133 ], [ 105.49856122562011, 34.717416981208771 ], [ 105.49406122584497, 34.718116981113837 ], [ 105.49136122544024, 34.721916980983963 ], [ 105.47776122510049, 34.728016980799055 ], [ 105.47136122583998, 34.727316980893988 ], [ 105.47226122597488, 34.722016981098875 ], [ 105.46946122545523, 34.720516981173773 ], [ 105.45226122547524, 34.732616980689158 ], [ 105.44306122569509, 34.736016980998897 ], [ 105.42686122596484, 34.74101698134865 ], [ 105.38676122575004, 34.751616980938991 ], [ 105.37676122594991, 34.754716980903993 ], [ 105.35856122572005, 34.754716980903993 ], [ 105.34406122524541, 34.760016980699163 ], [ 105.3221612252604, 34.76161698073912 ], [ 105.30856122581997, 34.760916980834054 ], [ 105.2830612252954, 34.752616981188794 ], [ 105.24566122548521, 34.754216981228751 ], [ 105.23296122528041, 34.751216981378661 ], [ 105.22746122525541, 34.747516980724129 ], [ 105.21016122516045, 34.746016980799084 ], [ 105.19746122585497, 34.750516980574218 ], [ 105.18836122529035, 34.756616981288687 ], [ 105.18286122526541, 34.757716980754083 ], [ 105.16836122569009, 34.767116980764115 ], [ 105.13736122514047, 34.767916980784094 ], [ 105.12366122558518, 34.770916980634183 ], [ 105.09036122509053, 34.773216980579264 ], [ 105.08686122556514, 34.775916980983936 ], [ 105.08366122548523, 34.776216981328673 ], [ 105.05906122509555, 34.782316981143822 ], [ 105.04226122557515, 34.780416980759128 ], [ 105.03626122587491, 34.778516981273697 ], [ 105.01986122591489, 34.770216980729117 ], [ 105.01626122537533, 34.765016981048916 ], [ 104.9961612256601, 34.75371698065419 ], [ 104.96256122572004, 34.740916981233738 ], [ 104.93886122546525, 34.73481698051927 ], [ 104.90976122530037, 34.728816980819033 ], [ 104.88696122518047, 34.728016980799055 ], [ 104.88426122567506, 34.729516980724156 ], [ 104.87786122551523, 34.731016980649201 ], [ 104.86786122571505, 34.738516981173802 ], [ 104.85416122526038, 34.755816981268708 ], [ 104.84686122586493, 34.758816981118855 ], [ 104.83136122514048, 34.759616981138834 ], [ 104.82136122534035, 34.76861698068916 ], [ 104.82126122522544, 34.794216981328702 ], [ 104.81856122572003, 34.797916981083858 ], [ 104.80306122589491, 34.799416981008903 ], [ 104.78856122542027, 34.792616981288688 ], [ 104.76846122570504, 34.796316981043901 ], [ 104.75936122514048, 34.803116980764116 ], [ 104.75656122552022, 34.809116981363672 ], [ 104.75016122536033, 34.816616980988954 ], [ 104.73916122531034, 34.839916980784096 ], [ 104.74366122508553, 34.848216981328676 ], [ 104.72446122550519, 34.860216980729149 ], [ 104.71996122573006, 34.86771698125375 ], [ 104.71986122561515, 34.875216980879031 ], [ 104.72986122541528, 34.899316980694152 ], [ 104.72986122541528, 34.903816981368664 ], [ 104.73066122543526, 34.921916980584228 ], [ 104.71596122563011, 34.945216981278691 ], [ 104.70956122547022, 34.964016981298698 ], [ 104.70546122525542, 34.968216980729153 ], [ 104.70126122582496, 34.973816980869003 ], [ 104.69396122553019, 34.97681698071915 ], [ 104.67846122570506, 34.984216981128839 ], [ 104.6645612259199, 35.015016980549262 ], [ 104.65176122560013, 35.028616980889012 ], [ 104.59686122546526, 35.04871698060424 ], [ 104.59586122521546, 35.05171698135365 ], [ 104.5839612259299, 35.055416981108863 ], [ 104.54286122546523, 35.057516980824062 ], [ 104.5328612256651, 35.058216980729128 ], [ 104.51996122523042, 35.062716981403639 ], [ 104.46036122509054, 35.095616980539262 ], [ 104.4457612254003, 35.101516981023906 ], [ 104.42926122532538, 35.104516980873996 ], [ 104.40086122596489, 35.105116980664206 ], [ 104.39816122556016, 35.103516980624192 ], [ 104.36156122577, 35.101116980564257 ], [ 104.34516122580999, 35.104816981218733 ], [ 104.28726122582498, 35.136116981213775 ], [ 104.26436122559016, 35.141916980684186 ], [ 104.26066122583495, 35.144916980534276 ], [ 104.24236122549024, 35.146316981243729 ], [ 104.22586122541526, 35.141716981353682 ], [ 104.19586122511549, 35.125716980953939 ], [ 104.1796612253853, 35.106016980799097 ], [ 104.12426122557514, 35.064316980544277 ], [ 104.11806122564514, 35.047716981253757 ], [ 104.11266122573505, 35.042416980559267 ], [ 104.09986122541528, 35.040016981398651 ], [ 104.0879612252304, 35.042216981228762 ], [ 104.07616122516049, 35.037616981338658 ], [ 104.07256122552019, 35.032316980644168 ], [ 104.07476122535036, 34.998416981258742 ], [ 104.06686122526537, 34.968316980844065 ] ],      //随机生成线的坐标集合
                 routePts:[],     //播放路径的坐标点集合
                 polyPositions:null,     //当前路径的坐标点
+                polyColor:null,      //当前线的颜色
                 curRouteIndex:1,        //当前路径走到的点
                 eChartsTitle:'',
                 bGifShow:false,     //动图是否显示
@@ -297,6 +302,7 @@
         },
         mounted:function(){
             this.initMap();
+            _.reverse(this.linePts);
         },
         methods:{
             //初始化地图
@@ -316,10 +322,10 @@
                         requestWaterMask: true
                     }),*/
                     imageryProvider: new Cesium.UrlTemplateImageryProvider({
-                        url: 'http://115.29.141.58:8098/MapData/gansu/{z}/{x}/{y}.png',
+                        url: 'http://115.29.141.58:8098/MapData/china/{z}/{x}/{y}.png',
                         tilingScheme: new Cesium.WebMercatorTilingScheme(),
                         minimumLevel: 0,
-                        maximumLevel: 11
+                        maximumLevel: 8
                     }),
                     terrainProvider:new Cesium.CesiumTerrainProvider({
                         url: 'http://115.29.141.58:8098/MapData/hongshuiDEM',
@@ -339,6 +345,15 @@
                     sceneMode: Cesium.SceneMode.SCENE3D//SCENE2D
                 };
                 g_viewer = new Cesium.Viewer('cesiumContainer', viewerOption);
+
+                //加载甘肃的影像
+                g_viewer.imageryLayers.addImageryProvider(
+                    new Cesium.UrlTemplateImageryProvider({
+                        url: 'http://115.29.141.58:8098/MapData/gansu/{z}/{x}/{y}.png',
+                        tilingScheme: new Cesium.WebMercatorTilingScheme(),
+                        minimumLevel: 0,
+                        maximumLevel: 11
+                    }));
 
                 //加载洪水淹没部分的影像
                 g_viewer.imageryLayers.addImageryProvider(
@@ -388,11 +403,20 @@
                     strokeWidth: 1,
                     markerSymbol: '?'
                 })
-                g_viewer.dataSources.add(river5);
+                     g_viewer.dataSources.add(river5);
 
                 g_viewer.clock.onTick.addEventListener(this.onCesiumTick);
 
                 g_viewer.scene.globe.baseColor = Cesium.Color.BLACK;
+
+                /*let rectangle = new Cesium.Rectangle(
+                    Cesium.Math.toRadians(73.66),
+                    Cesium.Math.toRadians(3.86),
+                    Cesium.Math.toRadians(135.05),
+                    Cesium.Math.toRadians(53.55));
+
+                g_viewer.scene.camera.flyTo({destination: rectangle});*/
+
                 g_viewer._cesiumWidget._creditContainer.style.display = "none";// 隐藏版权
             },
             //cesium自带的定时器事件
@@ -838,12 +862,17 @@ debugger;
                     duration:0.001
                 });
             },
-            //污染物沿着线进行演进
-            contaEvo:function(){
-                this.randomLineString2();
+            //飞行完成后沿着线进行演进
+            contaEvoFlyEnd:function(){
+                //g_viewer.scene.camera.moveEnd.removeEventListener(this.contaEvoFlyEnd);
 
                 this.timer = window.setInterval(() => {
                     if (this.curRouteIndex <= this.routePts.length) {
+
+                        if((this.curRouteIndex % 5) == 0){
+                            this.polyColor = new Cesium.Color(224/255, 118/255, 31/255,
+                                (this.routePts.length - this.curRouteIndex) /this.routePts.length);
+                        }
 
                         if(this.curRouteIndex == 100){
                             this.bShowFloodEvo = true;
@@ -902,11 +931,19 @@ debugger;
                     }
                 }, 10);
             },
+            //污染物沿着线进行演进
+            contaEvo:function(){
+                this.randomLineString2();
+
+                window.setTimeout(this.contaEvoFlyEnd,3000);
+                //g_viewer.scene.camera.moveEnd.addEventListener(this.contaEvoFlyEnd);
+            },
             //随机绘制线污染物
             randomLineString2:function () {
                 debugger;
+                g_viewer.scene.globe.depthTestAgainstTerrain = false;
                 g_viewer.entities.removeAll();
-                let lineStrings = turf.randomLineString(
+                /*let lineStrings = turf.randomLineString(
                     1,
                     {
                         bbox: this.bbox,
@@ -915,12 +952,12 @@ debugger;
                         max_rotation:Math.PI/100
                     }
                 );
-                this.linePts = lineStrings.features[0].geometry.coordinates;
+                this.linePts = lineStrings.features[0].geometry.coordinates;*/
 
                 let entity = g_viewer.entities.add({
                     polyline:{
                         positions:Cesium.Cartesian3.fromDegreesArray(
-                            _.flatten(lineStrings.features[0].geometry.coordinates)
+                            _.flatten(this.linePts)
                         ),
                         material:new Cesium.ColorMaterialProperty(
                             new Cesium.Color(0, 0, 1, 0.5)),
@@ -929,7 +966,24 @@ debugger;
                     }
                 });
 
-                g_viewer.flyTo(entity,{duration:0.001});
+                let rectangleChina = new Cesium.Rectangle(
+                    Cesium.Math.toRadians(73.66),
+                    Cesium.Math.toRadians(3.86),
+                    Cesium.Math.toRadians(135.05),
+                    Cesium.Math.toRadians(53.55));
+
+                g_viewer.scene.camera.flyTo({destination: rectangleChina});
+
+                g_viewer.scene.camera.moveEnd.addEventListener(this.floodEvoFlyEnd);
+                let rectangle = new Cesium.Rectangle(
+                    Cesium.Math.toRadians(104.066861),
+                    Cesium.Math.toRadians(34.544617),
+                    Cesium.Math.toRadians(105.959961),
+                    Cesium.Math.toRadians(35.146317));
+
+                g_viewer.scene.camera.flyTo({destination: rectangle});
+
+                //g_viewer.flyTo(entity,{duration:3});
 
                 this.routePts = [];
                 this.routePts.push(this.linePts[0]);
@@ -963,7 +1017,7 @@ debugger;
 
                 }
                 debugger;
-
+                this.polyColor = new Cesium.Color(224/255, 118/255, 31/255, 1);
                 let entity2 = g_viewer.entities.add({
                     polyline:{
                         positions:new Cesium.CallbackProperty(
@@ -973,7 +1027,9 @@ debugger;
                             image:'/images/test.png'
                         }),*/
                         material:new Cesium.ColorMaterialProperty(
-                            new Cesium.Color(224/255, 118/255, 31/255, 0.8)
+                            new Cesium.CallbackProperty(
+                                () => this.polyColor, false
+                            )
                         ),
                         clampToGround:true,
                         width:8,
@@ -1025,11 +1081,19 @@ debugger;
                     }
                 });
             },
-            //沿着线进行洪水演进
-            floodEvo:function(){
-                //禁止用户缩放
-                //g_viewer.scene.screenSpaceCameraController.enableZoom = false;
-                this.randomLineString();
+            //淹没分析时点击回到初始视角按钮
+            onHomeBtnClick:function(){
+                let rectangle = new Cesium.Rectangle(
+                Cesium.Math.toRadians(115.8784),
+                Cesium.Math.toRadians(39.9912),
+                Cesium.Math.toRadians(115.9614),
+                Cesium.Math.toRadians(40.0381));
+
+                g_viewer.scene.camera.flyTo({destination: rectangle});
+            },
+            //飞行完成后进行洪水演进
+            floodEvoFlyEnd:function(){
+                g_viewer.scene.camera.moveEnd.removeEventListener(this.floodEvoFlyEnd);
                 this.bGifShow = true;
                 this.timer = window.setInterval(() => {
                     if (this.curRouteIndex <= this.routePts.length) {
@@ -1050,7 +1114,7 @@ debugger;
                             this.initEChart();
                         }
 
-                        if(this.curRouteIndex == 300){
+                        if(this.curRouteIndex == 251){
                             if(this.timer){
                                 debugger;
                                 this.bShowFloodEvo = false;
@@ -1105,10 +1169,18 @@ debugger;
                     }
                 }, 10);
             },
+            //沿着线进行洪水演进
+            floodEvo:function(){
+                //禁止用户缩放
+                //g_viewer.scene.screenSpaceCameraController.enableZoom = false;
+                this.randomLineString();
+                //window.setTimeout(this.floodEvoFlyEnd,3000);
+            },
             //随机绘制线
             randomLineString:function () {
+                g_viewer.scene.globe.depthTestAgainstTerrain = false;
                 g_viewer.entities.removeAll();
-                let lineStrings = turf.randomLineString(
+                /*let lineStrings = turf.randomLineString(
                     1,
                     {
                         bbox: this.bbox,
@@ -1117,12 +1189,12 @@ debugger;
                         max_rotation:Math.PI/100
                     }
                 );
-                this.linePts = lineStrings.features[0].geometry.coordinates;
-
+                this.linePts = lineStrings.features[0].geometry.coordinates;*/
+                //debugger;
                 let entity = g_viewer.entities.add({
                     polyline:{
                         positions:Cesium.Cartesian3.fromDegreesArray(
-                            _.flatten(lineStrings.features[0].geometry.coordinates)
+                            _.flatten(this.linePts)
                         ),
                         material:new Cesium.ColorMaterialProperty(
                             new Cesium.Color(0, 0, 1, 0.5)),
@@ -1130,8 +1202,6 @@ debugger;
                         clampToGround:true
                     }
                 });
-
-                g_viewer.flyTo(entity,{duration:0.001});
 
                 this.routePts = [];
                 this.routePts.push(this.linePts[0]);
@@ -1164,7 +1234,24 @@ debugger;
                     }
 
                 }
-                debugger;
+
+                let rectangleChina = new Cesium.Rectangle(
+                    Cesium.Math.toRadians(73.66),
+                    Cesium.Math.toRadians(3.86),
+                    Cesium.Math.toRadians(135.05),
+                    Cesium.Math.toRadians(53.55));
+
+                g_viewer.scene.camera.flyTo({destination: rectangleChina});
+
+                g_viewer.scene.camera.moveEnd.addEventListener(this.floodEvoFlyEnd);
+                let rectangle = new Cesium.Rectangle(
+                    Cesium.Math.toRadians(104.066861),
+                    Cesium.Math.toRadians(34.544617),
+                    Cesium.Math.toRadians(105.959961),
+                    Cesium.Math.toRadians(35.146317));
+
+                g_viewer.scene.camera.flyTo({destination: rectangle});
+                //g_viewer.flyTo(entity);
 
                 let entity2 = g_viewer.entities.add({
                     polyline:{
@@ -1322,6 +1409,7 @@ debugger;
             },
             //点击了顶部菜单监测预警
             monitWaring:function(){
+                g_viewer.scene.globe.depthTestAgainstTerrain = false;
                 this.stopFloodAnaly();
                 if(this.timer){
                     window.clearInterval(this.timer);
